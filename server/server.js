62c02aaa7e4b1f00b268727a368b49e1ead0494f
KanoniPan <panevschi.ruslan@gmail.com>
2018-01-31 15:37:13 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -10,7 +11,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) =>{
-  //console.log(req.body);
+  // console.log(req.body);
   var todo = new Todo({
     text: req.body.text
   });
@@ -26,6 +27,22 @@ app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
   res.send({todos});
   }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
     res.status(400).send(e);
   });
 });
