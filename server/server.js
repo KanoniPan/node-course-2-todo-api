5e3d6c5a2b7bd4005f49dee60a72463d5c0965e8
KanoniPan <panevschi.ruslan@gmail.com>
2018-01-31 19:05:07 +0200

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -59,6 +59,7 @@ app.delete('/todos/:id', (req, res) => {
       return res.status(404).send();
     }
     if(todo){
+      // res.send({todo: Todo}); same code as below
     res.send({todo});
       }
   }).catch((e) => {
